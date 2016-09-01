fd698e83fd790c16d702bb92e4aa67fbf810e355
George Richmond <georgerichmond@gmail.com>
2016-09-01 16:22:05 +0100

    Nikkei Asian Review logo for chart 2 card

--- app/scripts/services/templateconfigprovider.js
+++ app/scripts/services/templateconfigprovider.js
@@ -751,7 +751,7 @@ angular.module('cardkitApp')
             }, {
               name: 'Ref Text',
               type: 'text',
-              text: 'FT.COM/COMPANIES',
+              text: 'FT.COM',
               controlsOrder: 3,
               fill: function() {
                 return  $scope.theme.xref;
@@ -915,7 +915,7 @@ angular.module('cardkitApp')
             }, {
               name: 'Reference Text',
               type: 'text',
-              text: 'FT.COM/COMPANIES',
+              text: 'FT.COM',
               controlsOrder: 3,
               fill: function() {
                 return $scope.theme.xref;
@@ -1055,28 +1055,28 @@ angular.module('cardkitApp')
               type: 'image',
               controlsOrder: 6,
               width: function() {
-                return $scope.size.gridSize * 2;
+                return $scope.size.gridSize * ($scope.theme.isNikkei? 10 : 2);
               },
               height: function() {
-                return $scope.size.gridSize * 2;
+                return $scope.size.gridSize * ($scope.theme.isNikkei ? 3 : 2);
               },
               src: function() {
-                return $scope.theme.images.logoSrc;
+                return $scope.theme.isNikkei ? $scope.theme.images.logoWideSrc : $scope.theme.images.logoSrc;
               },
               opacity: 1,
               x: function() {
-                return $scope.size.width - ($scope.size.gridSize * 3);
+                return $scope.size.width - ($scope.theme.isNikkei ? this.width($scope) : $scope.size.gridSize * 3);
               },
               y: function() {
-                var h = ($scope.size.gridSize);
-                return $scope.size.height - (this.height() + h);
+                var paddingTop = $scope.theme.isNikkei ? 0 : $scope.size.gridSize;
+                return $scope.size.height - (this.height($scope) + paddingTop);
               },
-              preserveAspectRatio: 'xMinYMin meet',
+              preserveAspectRatio: 'xMidYMid slice',
               draggable: false
             }, {
               name: 'Reference Text',
               type: 'text',
-              text: 'FT.COM/\nCOMPANIES',
+              text: 'FT.COM',
               controlsOrder: 3,
               fill: function() {
                 return $scope.theme.xref;
@@ -1093,7 +1093,7 @@ angular.module('cardkitApp')
                 return (w - w*0.25) + $scope.size.gridSize;
               },
               y: function() {
-                return $scope.size.height - ($scope.size.gridSize*2 + 2);
+                return $scope.size.height - ($scope.size.gridSize * ($scope.theme.isNikkei ? 4 : 2) + 2);
               },
               fontWeight: 500,
               draggable: false,
